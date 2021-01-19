b2d4f364ebce8c5560de5658618640e34c60a3ae
marcosomma <marcosomma.work@gmail.com>
2021-01-19 18:08:24 +0100

    fontsize in px

--- src/imports/creative.js
+++ src/imports/creative.js
@@ -2,7 +2,7 @@ import * as BABYLON from 'babylonjs'
 import 'babylonjs-loaders'
 export const importCreative = (scene) =>
   new Promise((resolve, reject) => {
-    BABYLON.SceneLoader.LoadAssetContainer('/assets/models/', `creative_2.obj`, scene, (assets) => {
+    BABYLON.SceneLoader.LoadAssetContainer('/assets/models/', `Creative.obj`, scene, (assets) => {
       assets.addAllToScene()
       resolve(assets.meshes)
     })
