9526bb087abf21cf85e52de06c1c84cd51454dff
marcosomma <marcosomma.work@gmail.com>
2021-01-20 14:53:50 +0100

    #2 Done

--- src/imports/creative.js
+++ src/imports/creative.js
@@ -1,9 +1,14 @@
 import * as BABYLON from 'babylonjs'
 import 'babylonjs-loaders'
+
+const SCALE = new BABYLON.Vector3(50, 50, 50)
 export const importCreative = (scene) =>
   new Promise((resolve, reject) => {
-    BABYLON.SceneLoader.LoadAssetContainer('/assets/models/', `Creative.obj`, scene, (assets) => {
+    BABYLON.SceneLoader.LoadAssetContainer('/assets/models/', `Creativity.obj`, scene, (assets) => {
       assets.addAllToScene()
+      assets.meshes.forEach((mesh) => {
+        mesh.rotation.y = -Math.PI / 2
+      })
       resolve(assets.meshes)
     })
   })
